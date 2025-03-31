b6d96d9c2e5a66e0c6a3d65ec9b8c5aec11e8f69
AlessandroRonconi <ronconi.alessandro46@gmail.com>
2025-03-31 16:16:21 +0200

    Add files via upload

--- src/it/uniroma3/diadia/giocatore/GiocatoreTest.java
+++ src/it/uniroma3/diadia/giocatore/GiocatoreTest.java
@@ -2,13 +2,32 @@ package it.uniroma3.diadia.giocatore;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GiocatoreTest {
+	private Giocatore player;
+	@BeforeEach
+	void setUp()throws Exception{
+		player = new Giocatore();
+	}
+	@Test
+	void testVerificaCfuIniziali() {
+		assertTrue(this.player.getCFU()==20);
+	}
+
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testCfuCambiati() {
+		this.player.setCFU(2);
+		assertFalse(this.player.getCFU()==20);
+	}
+	@Test
+	void testCfuCambiati2() {
+		this.player.setCFU(0);
+		assertTrue(this.player.getCFU()==0);
+		this.player.setCFU(9);
+		assertTrue(this.player.getCFU()==9);
 	}
 
 }
